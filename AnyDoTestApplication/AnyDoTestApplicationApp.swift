2c6359cdf331c522588ebe979cae34a3645889fd
Alexander Gaidukov <alexander.gaidukov@gmail.com>
2022-04-04 19:19:26 +0700

    Initial Commit

---
+++ AnyDoTestApplication/AnyDoTestApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyDoTestApplicationApp.swift
+//  AnyDoTestApplication
+//
+//  Created by Alexandr Gaidukov on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnyDoTestApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
